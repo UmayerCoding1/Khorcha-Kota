3644586b5f5339f615149beb84a2241b394792b1
umayerCoding1 <umayerpro175@gmail.com>
2025-04-20 23:43:23 +0600

    some error handle and code update

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors({
     origin: [
-        "http://localhost:5173"
+        "http://localhost:5173",
+        "https://khorcha-kota-client.vercel.app"
     ],
     credentials: true
 }));
